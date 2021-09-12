77c7dc096b7219e47211a532bcebaf24ef05e9ba
Maíra Matos <maira.matos@outlook.com.br>
2021-09-12 19:03:27 -0300

    add

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,11 +4,14 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
 let trakinUId = 'G-6S7FFW3WE6';
 // let trakinUId = UA-12345-6;
 
-function addaAalitics ( ) {
-    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+function addaAnalitics ( ) {
+    window.ga = window.ga || function(){(ga.q=ga.q||[]).push(arguments)}; 
+    ga.l=+new Date;
     ga('create', trakinUId, 'auto');
-    ga('send', 'pageview');
+    ga('send', 'pageview'); 
 }
+
