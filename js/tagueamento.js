2e27ae17ab0728ffd0ae970a584d196d078da2f1
Maíra Matos <maira.matos@outlook.com.br>
2021-09-13 00:15:06 -0300

    insert analytics to body

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -6,19 +6,21 @@
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
 
 function addAnalitics( ) {
-    window.ga = window.ga || function(){(ga.q=ga.q||[]).push(arguments)}; 
-    ga.l=+new Date;
-    ga('create', 'G-6EL47LX6DE', 'auto');
-    ga('send', 'pageview'); 
-    console.log("Iniciar GA.");
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview');
 }
 function sendPageVisualization(){
     ga('send', {
-        'hitType': 'pageview',
+        'hitType': 'page_view',
         'page': location.pathname,
         'title': document.title,        
         'hitCallback' : function () {
-            alert("Event received");
+            console.log("Event received");
     }
     });
 }
